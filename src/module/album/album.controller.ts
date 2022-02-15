24a96e8683123fb24a095e224d48f92ad76a6f5f
yangxinxiang <yangxinxiang@100tal.com>
2022-02-15 18:29:36 +0800

    feat: 练习typeorm中的queryBuilder

--- src/module/album/album.controller.ts
+++ src/module/album/album.controller.ts
@@ -21,6 +21,49 @@ export class AlbumController{
         return rst
     }
 
+    @Get("getPictureViaQB") // 静态路由方式访问和传参： http://localhost:3000/album/getPictureViaQB?id=5
+    async getPictureByIdViaQB(@Query("id") id: string){
+        console.log("getPicture :: enter, id = ", id)
+        const rst = await this.albumService.getPictureByIdViaQB(parseInt(id))
+        return rst
+    }
+
+    @Get("qbInsert")
+    async testQueryBuilderInsert() {
+        const rst = await this.albumService.insertPictureViaQB()
+        return rst;
+    }
+
+    @Get("qbUpdate")
+    async testQueryBuilderUpdate() {
+        // const rst = await this.albumService.insertPictureViaQB()
+        const rst = await this.albumService.updatePictureViaQB(5)
+        return rst;
+    }
+
+    @Get("getRawOnePicture")
+    async getRawOnePicture(@Query("id") id:string) {
+        const rst = await this.albumService.getRawOnePicture(parseInt(id))
+        return rst;
+    }
+
+    @Get("getRawManyPictures")
+    async getRawManyPictures(){
+        const rst = this.albumService.getRawManyPictures()
+        return rst;
+    }
+
+    @Get("getRawManyPictures2")
+    async getRawManyPictures2(){
+        const rst = this.albumService.getRawManyPictures2(["tp-yxx-02", "tp-yxx-04"])
+        return rst;
+    }
+
+    @Get("getRawManyPictures3")
+    async getRawManyPictures3(){
+        const rst = this.albumService.getRawManyPictures3()
+        return rst;
+    }
     @Post("create")
     async create(@Body("album") albumInfo: AlbumCreateDto, @Body("pictures") pics:PictureCreateDto[]){
         const rst = await this.albumService.create(albumInfo, pics);
--- src/module/album/album.service.ts
+++ src/module/album/album.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@nestjs/common"
 import {InjectRepository, } from "@nestjs/typeorm"
-import {Repository} from "typeorm"
+import {getConnection, Repository} from "typeorm"
 import {Album, Picture} from "../../entities"
 import {AlbumCreateDto, PictureCreateDto} from "./dto"
 @Injectable()
@@ -20,6 +20,92 @@ export class AlbumService{
     async getPictureById(id:number) {
         const rst = this.pictureRepository.findOne(id, {relations:["albums"]})
         return rst 
+    }
+
+    // 练习通过queryBuilder操作数据库
+    async getPictureByIdViaQB(id:number){
+        // const rst = await this.pictureRepository
+        // .createQueryBuilder("pic")// 别名
+        // .where("pic.id = :id", {id})
+        // .getOne()
+        // return rst
+
+        const rst = await this.pictureRepository
+        .createQueryBuilder()// 别名
+        .select("pic")
+        .from(Picture, "pic")
+        .where("pic.id = :id", {id})
+        .getOne()
+        return rst
+    }
+
+    // 练习通过queryBuilder来插入数据
+    async insertPictureViaQB(){
+        const p1:Picture = new Picture()
+        p1.author = "t-author-via-qb-03"
+        p1.name = "一张漂亮的照片3";
+
+        const p2:Picture = new Picture()
+        p2.author = "t-author-via-qb-04"
+        p2.name = "一张漂亮的照片4";
+        const rst = await this.pictureRepository
+        .createQueryBuilder()
+        .insert()
+        .into(Picture)
+        .values([
+            // {author : "t-author-via-qb-01", name:"一张漂亮的照片1"},
+            // {author : "t-author-via-qb-02", name:"一张漂亮的照片2"},
+            p1,p2
+        ])
+        .execute()
+        return rst;
+    }
+
+    // 练习通过queryBuilder来更新数据
+    async updatePictureViaQB(id:number) {
+        const rst = this.pictureRepository.createQueryBuilder()
+        .update(Picture)
+        .set({name:"一张漂亮的照片4~~~222"})
+        .where("id=:id", {id})
+        .execute()
+    }
+
+    // 练习通过queryBuilder使用mysql更丰富的功能，使用内置函数
+    async getRawOnePicture(id, author="tp-yxx-04") {
+        const rst = this.pictureRepository.createQueryBuilder("pic")
+        // .select("pic.id as ppid, pic.name as ppname")
+        .select("COUNT(pic.author)", "count")
+        .where("pic.author = :author", {author})
+        .getRawOne()
+        return rst;
+    }
+
+    // 练习通过queryBuilder使用 where限定语句
+    async getRawManyPictures(author="tp-yxx-04") {
+        const rst = this.pictureRepository.createQueryBuilder("pic")
+        // .select()
+        .where("pic.author = :author", {author})
+        .getRawMany()
+        return rst;
+    }
+
+    // 练习通过queryBuilder使用 where限定语句
+    async getRawManyPictures2(authors=["tp-yxx-04"]) {
+        const rst = this.pictureRepository.createQueryBuilder("pic")
+        // .select()
+        .where("pic.author IN(:...authors)", {authors})
+        .getMany()
+        return rst;
+    }
+
+    async getRawManyPictures3() {
+        const rst = this.pictureRepository.createQueryBuilder("pic")
+        // .select()
+        .where("pic.author = :author", {author: "tp-yxx-02"})
+        .orWhere("pic.author = :author2", {author2: "tp-yxx-04"}) // 注意，同一个语句中，使用相同名字的变量，后面的会覆盖前面的。所以这里要用author2
+        .getMany()
+        
+        return rst;
     }
     
     async create(albumInfo: AlbumCreateDto,  picInfoes:PictureCreateDto[]){
